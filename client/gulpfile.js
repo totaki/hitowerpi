c4634fae38a7af15ecdd6472cc4b6b8738fec907
totaki <karagabul@gmail.com>
2015-11-08 20:36:15 +0300

    Add #pins page and make creator for pins

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -2,6 +2,7 @@
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
+    rigger = require('gulp-rigger'),
     browserSync = require("browser-sync"),
     reload = browserSync.reload;
 
@@ -35,7 +36,7 @@ var path = {
         js: 'src/js/**/*.js',
         style: 'src/style/**/*.scss',
         img: 'src/img/**/*.*',
-        fonts: 'src/fonts/**/*.*'
+        fonts: 'src/fonts/**/*.*'   
     },
     clean: './build'
 };
@@ -46,25 +47,39 @@ gulp.task('webserver', function () {
 
 gulp.task('html:build', function () {
     gulp.src(path.src.html)
+        .pipe(rigger())
         .pipe(gulp.dest(path.build.html))
         .pipe(reload({stream: true}));
 });
 
-gulp.task('sass', function() {
+gulp.task('js:build', function () {
+    gulp.src(path.src.js)
+        .pipe(rigger())
+        .pipe(gulp.dest(path.build.js))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('sass:compile', function() {
     return gulp.src(path.src.style)
         .pipe(sass())
-        .pipe(gulp.dest(path.build.css));
+        .pipe(gulp.dest(path.build.css))
+        .pipe(reload({stream: true}));
 });
 
 gulp.task('watch', function(){
     gulp.watch([path.watch.html], function(event, cb) {
         gulp.start('html:build');
     });
-    gulp.watch(path.watch.style, ['sass']);
+    gulp.watch([path.watch.js], function(event, cb) {
+        gulp.start('js:build');
+    });
+    gulp.watch(path.watch.style, ['sass:compile']);
 })
 
 gulp.task('build', [
-    'html:build'
+    'html:build',
+    'js:build',
+    'sass:compile'
 ]);
 
 
